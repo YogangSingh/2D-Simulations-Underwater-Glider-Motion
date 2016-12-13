9abb90ad02be168e2d4c4afa54b35d7545e838b4
Yogang Singh <yogangsingh@gmail.com>
2016-12-13 16:49:29 +0000

    SLOCUM 2D

---
+++ Model3d.m
@@ -0,0 +1,245 @@
+function ret=Model3d(t,y)
+
+
+%% Import glider vairables
+
+load('glider_vars.mat');
+
+%% Define terms
+
+b       = y(1:3);                 % Position in earth frame
+Omega   = y(4:6);                 % Angular rate
+v       = y(7:9);                 % Velocity
+rp      = y(10:12);               % Position of primary moving mass
+rs      = y(13:15);               % Position of secondary moving mass
+rb      = y(16:18);               % Position of ballast mass
+Pp      = y(19:21);               % Linear momentum of primary moving mass
+Ps      = y(22:24);               % Linear momentum of secondary moving mass
+%Pb     = y(25:27);               % Linear momentum of ballast moving mass
+mb      = y(28);                  % Ballast mass
+theta   = y(29);                  % Pitch
+alpha = atan(v(3)/v(1));          % Angle of attack
+
+%% Check on ballast mass
+
+if glide_dir == 'U'
+    if mb <= mb_d
+        mb = mb_d;              % Turn off ballast when desired mass is obtained
+    end    
+elseif glide_dir == 'D'
+    if mb >= mb_d
+        mb = mb_d;              % Turn off ballast when desired mass is obtained
+    end  
+end
+ 
+%% Position Vectors
+
+% Limit the longitudinal position of moving masses
+if glide_dir == 'U'             % During upward glide primary moving mass moves in negative e1 direction
+    if rp(1) <= rp1_d
+        rp(1) = rp1_d;
+    end
+    if rs(1) <= rs1_d
+        rs(1) = rs1_d;
+    end
+elseif glide_dir == 'D'         % During downward glide primary moving mass moves in positive e1 direction
+    if rp(1) >= rp1_d
+        rp(1) = rp1_d;
+    end 
+    if rs(1) >= rs1_d
+        rs(1) = rs1_d;
+    end 
+end
+
+% Primary moving mass
+rp_c = [ 0      -rp(3)  rp(2)
+         rp(3)  0       -rp(1)
+        -rp(2)  rp(1)   0  ];
+    
+% Secondary moving mass
+rs_c = [ 0      -rs(3)  rs(2)
+         rs(3)  0       -rs(1)
+        -rs(2)  rs(1)   0  ];
+
+% Ballast mass
+rb_c = [ 0      -rb(3)  rb(2)
+         rb(3)  0       -rb(1)
+        -rb(2)  rb(1)   0  ]; 
+    
+    
+%% Transformation matrices 
+
+R = [cos(yaw)*cos(theta)    (-sin(yaw)*cos(phi) + cos(yaw)*sin(theta)*sin(phi)) (sin(yaw)*sin(phi) + cos(yaw)*cos(phi)*sin(theta))
+     sin(yaw)*cos(theta)    (cos(yaw)*cos(phi) + sin(phi)*sin(theta)*sin(yaw))  (-cos(yaw)*sin(phi) + sin(theta)*sin(yaw)*cos(phi))
+     -sin(theta)            cos(theta)*sin(phi)                                 cos(theta)*cos(phi)];
+
+RT = R';
+
+%% Force and Torque terms
+
+L = (KL0 + KL*((alpha)))*(v(1)^2 + v(3)^2);                      % Lift force                         
+D = (KD0 + KD*((alpha))^2)*(v(1)^2 + v(3)^2);                    % Drag force
+MDL = (KM0 + KM*((alpha)))*(v(1)^2 + v(3)^2) ...                 % Moment
+      + KOmega1_2*Omega(2) + KOmega2_2*(Omega(2)^2);
+
+F_ext = [-D 0 -L]';                                          % Total external force
+T_ext = [0 MDL 0]';                                          % Total external mmoment
+
+%% Internal Mass Velocity Vectors
+
+% Limit the rate of movement of primary moving mass
+if glide_dir == 'U'
+    rp_dot = (1/mbar)*Pp - v - cross(Omega, rp);             % rp_dot in upward glide
+    if ms > 0                                                
+       rs_dot = (1/ms)*Ps - v - cross(Omega, rs);            % rs_dot in upward glide
+    else
+       rs_dot = [0 0 0]'; 
+    end
+    
+    if rp(1) <= rp1_d                                        % When desired value reached, set rate = 0
+        rp_dot = [0 0 0]';
+    end
+    if rs(1) <= rs1_d                                        % When desired value reached, set rate = 0
+        rs_dot = [0 0 0]';
+    end
+elseif glide_dir == 'D'
+    rp_dot = (1/mbar)*Pp + v - cross(Omega, rp);             % rp_dot in upward glide
+    if ms > 0                                                
+       rs_dot = (1/ms)*Ps + v - cross(Omega, rs);            % rs_dot in upward glide
+    else
+       rs_dot = [0 0 0]'; 
+    end
+    
+    if rp(1) >= rp1_d                                        % When desired value reached, set rate = 0
+        rp_dot = [0 0 0]';
+    end
+    if rs(1) >= rs1_d                                        % When desired value reached, set rate = 0
+        rs_dot = [0 0 0]';
+    end
+end
+
+rb_dot = [0 0 0]';                                           % rb_dot
+
+%% Momentum Matrices
+
+Pb = mb*(v + cross(Omega,rb) + rb_dot);                     % Linear momentum of ballast mass
+
+%% Calculate net buoyancy
+
+m0 = mh + mw + mb + ms + mbar - m;
+
+%% Control Inputs
+
+% Set acceleration input of primary moving mass
+if glide_dir == 'U'
+    wp1 = -0.02;            % wp1 in upward glide (m/s^2)
+elseif glide_dir == 'D' 
+    wp1 = 0.02;             % wp1 in downward glide (m/s^2)
+end
+wp2 = 0;                    % wp2
+wp3 = 0;                    % wp3
+
+wp = [wp1                   % (w1) Accleration input of movable point mass mbar along body axis e1
+      wp2                   % (w2) Accleration input of movable point mass mbar along body axis e2
+      wp3];                 % (w3) Accleration input of movable point mass mbar along body axis e3
+
+% Accleration input of ballast point mass mb in body coordinates
+wb = 0;  
+ 
+% Check on ballast rate
+if glide_dir == 'U'
+    if mb <= mb_d
+        u_ballast_rate = 0;             % Turn off ballast when desired mass is obtained
+    end    
+elseif glide_dir == 'D'
+    if mb >= mb_d
+        u_ballast_rate = 0;             % Turn off ballast when desired mass is obtained
+    end  
+end
+
+%% Control Transformation
+
+F = [ (Minv - rp_c*Jinv*rp_c + (I3/mbar))       (Minv - rp_c*Jinv*rb_c)               
+      (Minv - rb_c*Jinv*rp_c)                   (Minv - rb_c*Jinv*rb_c + (I3/mb))];    
+
+H = inv(F);
+
+Zp = -Minv*[cross((M*v + Pp + Pb),Omega) + m0*g*RT*k + F_ext] - cross(Omega,rp_dot) ...
+      -Jinv*cross([cross((J*Omega + rp_c*Pp + rb_c*Pb),Omega) + cross((M*v),v) + T_ext ...
+      + cross(cross(Omega,rp),Pp) + cross(cross(Omega,rb),Pb) ...
+      + (mbar*rp_c + mb*rb_c)*g*RT*k],rp);
+
+Zb = -Minv*[cross((M*v + Pp + Pb),Omega) + m0*g*RT*k + F_ext] - cross(Omega,rb_dot) ...
+     -Jinv*cross([cross((J*Omega + rp_c*Pp + rb_c*Pb),Omega) + cross((M*v),v) + T_ext ...
+     + cross(cross(Omega,rp),Pp) + cross(cross(Omega,rb),Pb) ...
+     + (mbar*rp_c + mb*rb_c)*g*RT*k],rb);
+  
+ u = H*[(-Zp + wp)
+        (-Zb + wb)];
+    
+ %% Control Inputs
+ 
+ if glide_dir == 'U'
+     
+    if rp(1) <= rp1_d
+        u_bar = [0 0 0]';
+    else
+        u_bar = [-0.18 0 0]';
+        %u_bar = [u(1) 0 0]';
+    end 
+    
+    if rs(1) <= rs1_d
+        us    = [0 0 0]';
+    else
+        us    = [-0.18 0 0]';
+    end
+     
+    if ms == 0                              % If secondary mass doed not exist
+        us    = [0 0 0]';
+    end
+    
+ elseif glide_dir == 'D'
+    
+    if rp(1) >= rp1_d
+        u_bar = [0 0 0]';
+    else
+        u_bar = [0.18 0 0]';
+        %u_bar = [u(1) 0 0]';
+    end 
+    
+    if rs(1) >= rs1_d
+        us    = [0 0 0]';
+    else
+        us    = [0.18 0 0]';
+    end
+    
+    if ms == 0                              % If secondary mass doed not exist
+        us    = [0 0 0]';
+    end
+ end
+ 
+ ub = [0 0 0]';
+
+%% Equations of motion
+
+T_bar      = cross((J*Omega + rp_c*Pp + rs_c*Ps + rb_c*Pb), Omega) + cross((M*v),v) ...
+             + cross(cross(Omega, rp), Pp) + + cross(cross(Omega, rs), Ps) + + cross(cross(Omega, rb), Pb) + (mbar*rp_c + ms*rs_c + mb*rb_c)*g*RT*k ...
+             + T_ext - rp_c*u_bar - rs_c*us - rb_c*ub;
+
+F_bar      = cross((M*v + Pp + Ps + Pb), Omega) + m0*g*RT*k + F_ext - u_bar - us - ub;
+
+b_dot      = R*v;
+Omega_dot  = Jinv*T_bar;
+v_dot      = Minv*F_bar;
+% rp_dot     = (1/mbar)*Pp - v - cross(Omega, rp);
+% rs_dot     = (1/ms)*Pd - v - cross(Omega, rs);
+% rb_dot     = (1/mb)*Pb - v - cross(Omega, rb);
+Pp_dot     = u_bar;
+Ps_dot     = us;
+Pb_dot     = ub;
+mb_dot     = u_ballast_rate;
+theta_dot  = Omega(2);
+
+%% Return
+
+ret = [b_dot' Omega_dot' v_dot' rp_dot' rs_dot' rb_dot' Pp_dot' Ps_dot' Pb_dot' mb_dot theta_dot]';
---
+++ Sim3D.m
@@ -0,0 +1,217 @@
+%% Set Glider Parameters
+
+clear;                      % Clear workspace
+clc;                        % Clear screen              
+
+%% Set glider trajectory and other inputs
+
+
+Xi_d = [deg2rad(-25),deg2rad(25),deg2rad(-25),deg2rad(25)];      % Glider trajectory by specifying desired glide path angles
+V_d = 0.3;                  % Glider speed in the vertical plane
+rp3 = 0.05;                 % Fixed position of primary moving mass in body coordinate along e3 axis
+rs3 = 0.00;                 % Fixed position of secondary moving mass in body coordinate along e3 axis
+rb1 = 0.00;                 % Fixed position of ballast mass in body coordinate along e1 axis
+rb3 = 0.00;                 % Fixed position of ballast mass in body coordinate along e3 axis
+u_ballast_rate = 0.0250;    % Ballast rate input (kg/s) (Magnitude only, sign is assigned later)
+phi = 0;                    % Glider roll angle
+yaw = 0;                    % Glider yaw angle
+theta0 = 0.4;               % Initial pitch angle
+
+%% Constants
+
+g = 9.816;                  % Acceleration due to gravity
+I3 = eye(3);                % Identity matrix (3x3)
+Z3 = zeros(3);              % Zero matric (3x3)
+
+i = [1 0 0]';               % Unit vector alog earth frame x-axis
+j = [0 1 0]';               % Unit vector alog earth frame y-axis
+k = [0 0 1]';               % Unit vector alog earth frame z-axis
+
+%% Mass Properties
+
+mh = 40;                        % Hull mass
+mbar = 9;                       % Primary moving inernal mass
+ms = 0;                         % Secondary moving inernal mass
+mb = 1.0;                       % Variable ballast mass     
+mw = 0;                         % Fixed point mass
+mv = mh + mw + ms + mb + mbar;  % Total vehicle mass
+m = 50;                         % Displacement
+m0 = mv - m;                    % Buoyancy
+
+mf1 = 5;                        % Added mass term
+mf2 = 60;                       % Added mass term
+mf3 = 70;                       % Added mass term     
+Mf = diag([mf1, mf2, mf3]);     % Added mass matrix
+
+M = mh*I3 + Mf;                 % Total mass
+Minv = inv(M);
+m1 = M(1,1);    
+m2 = M(2,2);
+m3 = M(3,3);
+
+j1 = 4;                         % Inertia term  
+j2 = 12;                        % Inertia term
+j3 = 11;                        % Inertia term
+J = diag([j1, j2, j3]);         % Total intertia
+Jinv = inv(J);
+
+%% Force and Moment Coefficients 
+
+KL = 132.5;                 % Lift coefficient
+KL0 = 0;                    % Lift coefficient
+
+KD = 25;                    % Drag Coefficient
+KD0 = 2.15;                 % Drag Coefficient
+
+KM = -100;                  % Moment Coefficient
+KM0 = 0;                    % Moment Coefficient
+
+KOmega1_2 = -50;            % Rotational Damping Coefficient
+KOmega2_2 = -50;            % Rotational Damping Coefficient
+
+%% Admissible values of xi_d
+
+lim1 = rad2deg(atan(2*(KD/KL)*((KL0/KL) + nthroot(((KL0/KL)^2) + (KD0/KD), 2))));
+lim2 = rad2deg(atan(2*(KD/KL)*((KL0/KL) - nthroot(((KL0/KL)^2) + (KD0/KD), 2))));
+fprintf('Admissible values of xi_d     = (-90, %f) U (%f, 90)', lim2, lim1);
+
+%% Calculate glider trajectory
+
+len = length(Xi_d);
+t = [];
+r = [];
+
+for i=1:len
+    fprintf('\n\nIteration %f\n',i);
+
+    %% Desired value of glide angle
+
+    xi_d = Xi_d(i);
+    fprintf('Chosen value of xi_d          = %f degrees\n', rad2deg(xi_d));
+ 
+    %% Set glide direction & ballast rate
+    % Glide direction (U = Upwards, D = Downwards)
+
+    if xi_d > 0
+        glide_dir = 'U';
+        disp('Glider direction: Upward');
+        u_ballast_rate = -abs(u_ballast_rate);          % Negative ballast rate for upward glide
+    elseif xi_d < 0
+        glide_dir = 'D';
+        disp('Glider direction: Downward');
+        u_ballast_rate = abs(u_ballast_rate);           % Positive ballast rate for downward glide
+    end
+  
+    %% Desired angle of attack
+
+    alpha_d = (1/2)*(KL/KD)*(tan(xi_d))*(-1 + nthroot(1 - 4*(KD/(KL^2))*(cot(xi_d))*(KD0*cot(xi_d) + KL0),2));
+    fprintf('Desired valued of alpha_d     = %f degrees\n', rad2deg(alpha_d));
+
+    %% Desired velocity in body frame
+
+    v1_d = V_d*cos(alpha_d);
+    v3_d = V_d*sin(alpha_d);
+
+    %% Desired ballast mass
+
+    mb_d = (m - mbar - mh - ms -mw) + (1/g)*( (-sin(xi_d))*(KD0 + KD*(alpha_d^2)) + (cos(xi_d))*(KL0 + KL*(alpha_d)))*(V_d^2);
+    fprintf('Desired valued of mb_d        = %f kg\n', mb_d);
+
+    %% Desired position of longitudinal moving masses
+    
+    lambda = 1;
+    theta_d = alpha_d + xi_d;
+    
+    rp1_d = (1/(mbar + lambda*ms))*(-mb_d*rb1 -tan(theta_d)*(mbar*rp3 + ms*rs3 + mb*rb3) + (1/(g*cos(theta_d)))*((mf3 - mf1)*v1_d*v3_d + (KM0 + KM*alpha_d)*(V_d^2)));
+    rs1_d = lambda*rp1_d;
+    
+    fprintf('Desired valued of rp1_d       = %f m\n', rp1_d);
+    fprintf('Desired valued of rs1_d       = %f m\n', rs1_d);
+    
+    %% Save workspace
+    
+    save('glider_vars.mat');
+
+    %% Set initial conditions for ODE solver
+
+    % Initial params in first in the first glide
+    if i == 1
+        y0 = [[0 0 0]'               % b
+              [0 0 0]'               % Omega
+              [v1_d 0 v3_d]'         % Velocity
+              [rp1_d 0 rp3]'         % Position of primary moving mass
+              [rs1_d 0 rs3]'         % Position of secondary moving mass
+              [rb1 0 rb3]'           % Position of ballast mass
+              [0 0 0]'               % Pp
+              [0 0 0]'               % Ps
+              [mb*v1_d 0 mb*v3_d]'   % Pb
+              mb_d                   % Ballast mass
+              theta0];               % Pitch angle
+    else
+        l_idx = length(r);
+        y0 = [r(l_idx,1:3)'          % b
+              r(l_idx,4:6)'          % Omega
+              r(l_idx,7:9)'          % Velocity
+              r(l_idx,10:12)'        % Position of primary moving mass
+              r(l_idx,13:15)'        % Position of secondary moving mass
+              r(l_idx,16:18)'        % Position of ballast mass
+              r(l_idx,19:21)'        % Pp
+              r(l_idx,22:24)'        % Ps
+              r(l_idx,25:27)'        % Pb
+              r(l_idx,28)'           % Ballast mass
+              r(l_idx,29)]';         % Pitch angle
+    end
+    
+    %% Solve ODE
+
+    tspan = [(i-1)*375 i*375];
+    [T, R] = ode45(@Model3d,tspan,y0);
+    t = [t;T];
+    r = [r;R];
+end
+
+%% Plot results 
+
+h = figure;
+set(h, 'Position', [100 0 600 650]);
+
+% Depth
+subplot(5,1,1);
+plot(t, r(:,3));
+set(gca,'YDir','reverse');
+ylabel('Depth (m)','interpreter','latex');
+
+% Pitch angle
+subplot(5,1,2);
+plot(t, r(:,29)*180/pi);
+ylabel('$\theta$ (deg)','interpreter','latex');
+
+% Omega_2
+subplot(5,1,3);
+plot(t,r(:,5)*180/pi);
+ylabel('$\Omega_2$ (deg/s)','interpreter','latex');
+
+% Velocity
+V = (r(:,7).^2 + r(:,9).^2).^0.5;
+subplot(5,1,4);
+plot(t,V);
+ylabel('Velocity (m/s)','interpreter','latex');
+
+% Angle of attack
+subplot(5,1,5);
+alpha = atan(r(:,9)./r(:,7))*180/pi;
+plot(t,alpha);
+xlabel('Time (Seconds)');
+ylabel('$\alpha$ (deg)','interpreter','latex');
+
+% Title
+set(gcf,'NextPlot','add');
+axes;
+h = title(sprintf('Model Validation Study on reference Glider\n Vertical Plane Simulation'));
+set(gca,'Visible','off');
+set(h,'Visible','on');
+
+% Save to disk
+hgexport(gcf, 'Glider_VerticalPlane_Results.jpg', hgexport('factorystyle'), 'Format', 'jpeg');
+saveas(gcf, 'Glider_VerticalPlane_Results','fig');
+save('Glider_VerticalPlane_Results.mat');
